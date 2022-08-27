d148358493d1e0482f19ef0882eb45a67d59147c
Andrea Vendrame <andrea.risk@gmail.com>
2022-08-27 08:34:23 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -94,6 +94,41 @@ function App() {
         ))}
       </div>
       <WithdrawComponent key={accountAddress} connected_account={accountAddress} />
+      <div class="mt-5 pt-5 pb-5 footer">
+        <div class="box_container_padded">
+          <div class="d-flex justify-content-end">
+            <div class="wrapper">
+              <h2>Skybreach tools - Land Block Sale
+
+              </h2>
+              <div class="padding_20">
+                <p class="pr-5 text-white-50">Skybreach tools - Land Block Sale (also shortened LBS) is a free dApp that has the aim to add a new feature by giving to the project users the possibility to sell Skybreach metaverse lands in block directly on-chain and without trust a third human part.</p>
+              </div>
+            </div>
+            <div class="col-lg-3 col-xs-12 links">
+              <h4 class="mt-lg-0 mt-sm-3">Links</h4>
+              <ul class="m-0 p-0">
+                <li>- <a href="https://github.com/qwertyuiop9/skybreach">Github repository</a></li>
+                <li>- <a href="https://twitter.com/vendrame_and">Twitter</a></li>
+                <li>- <a href="https://www.superrisknft.com/">Website</a></li>
+                <li>- <a href="https://www.instagram.com/s_superrisk/">Instagram</a></li>
+                <li>- <a href="https://superrisk-studio.gitbook.io/skybreach-land-block-sale/">Gitbook</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4 col-xs-12 location">
+              <h4 class="mt-lg-0 mt-sm-4">Contacts</h4>
+              <p>Mail - superrisknft@gmail.com</p>
+              <p>Discord - superrisk#5988</p>
+              <p>Telegram - @vendrame_and</p>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col copyright">
+              <p class="padding_20"><small class="text-white-50">© 2022. All Rights Reserved.</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 

b6362b4a26cea747a6ae86eae9170f7be805e57b
FraArb <francesco.arbilli@libero.it>
2023-01-27 17:01:31 +0100

    Building home page and navigation buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState, useRef } from 'react';
 
-import { Box, AppBar, Typography } from '@mui/material';
+import { Box, AppBar, Typography, Button } from '@mui/material';
 import MButton from '@mui/material/Button';
 
 import { readLandBlockSC, landBlockCA, smartContractXCRMRK } from './components/LandBlockSale';
@@ -19,14 +19,100 @@ import HandleOffers from './components/HandleOffers';
 import DepositedLandCard from './components/DepositedLandCard';
 
 //added react router for more pages
-import {BrowserRouter,Routes,Route, Link} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useLocation, redirect  } from 'react-router-dom';
+
 
 function App() {
 
-  
+  // Account state
   const [currentAccount, setCurrentAccount] = useState("");
   var activeOffersNumber = useRef(-1);
 
+  // Appbar 
+  function SkybreachAppBar() {
+
+    return (
+      <AppBar position="static" sx={{ height: 130, backgroundColor: "#282c34", boxShadow: 24 }}>
+
+        <Box display="inline-flex" alignItems="center" justifyContent="space-between" sx={{ height: 100, mt: 3 }} >
+          <Box display='inline-flex' sx={{ ml: 4, alignItems: "center" }}>
+            <Box sx={{ minWidth: 100, maxWidth: 150, minHeight: 100, maxHeight: 150, mt: 2, elevation: 5, borderRadius: 100, boxShadow: 12, }} >
+              <img src={SRSlogo} alt="SRS logo" class="img-fluid" />
+            </Box>
+            <Box >
+              <Typography sx={{ ml: 2, fontWeight: 'bold', fontSize: 'clamp(26px, 4vw, 40px)', textAlign: 'left' }} variant='h4' >
+                Skybreach tools
+              </Typography>
+              <Typography sx={{ ml: 2, mb: 3, fontWeight: 'normal', fontSize: 'clamp(18px, 4vw, 30px)', textAlign: 'left' }} variant='h5'>
+                Land Block Sale
+              </Typography>
+            </Box>
+            <Box sx={{ ml: 5, mb: 2 }}>
+              {updateConnectedWallet()}
+            </Box>
+          </Box>
+
+          <Box sx={{ minWidth: 50 }} />
+
+          <Box display="inline-flex" gap={3} sx={{ mr: 5, mb: 2, alignItems: "center", justifyContent: "flex-end", minHeight: 10 }}>
+            <Button className='blueGradientButton blueGradientButton--navigation' variant='contained' size='large' sx={{ width: 200, maxHeight: 100, borderRadius: 10 }}>
+              <Link to={'/Landmanagement'} style={routeLinkStyle}>
+                Land Management
+              </Link>
+            </Button>
+            <Button className='blueGradientButton blueGradientButton--navigation' variant='contained' size='large' sx={{ width: 200, maxHeight: 100, borderRadius: 10 }}>
+              <Link to={'/Offermarket'} style={routeLinkStyle}>
+                Offer
+                <br />
+                Market
+              </Link>
+            </Button>
+          </Box>
+        </Box>
+      </AppBar>
+    );
+  }
+
+  function SkybreachFooter() {
+    return (
+      <div class="mt-5 pt-5 pb-5 footer">
+        <div class="box_container_padded">
+          <div class="d-flex justify-content-end">
+            <div class="wrapper">
+              <h2>Skybreach tools - Land Block Sale
+              </h2>
+              <div class="padding_20">
+                <p class="pr-5 text-white-50">Skybreach tools - Land Block Sale (also shortened LBS) is a free dApp that has the aim to add a new feature by giving to the project users the possibility to sell Skybreach metaverse lands in block directly on-chain and without trust a third human part.</p>
+              </div>
+            </div>
+            <div class="col-lg-3 col-xs-12 links">
+              <h4 class="mt-lg-0 mt-sm-3">Links</h4>
+              <ul class="m-0 p-0">
+                <li>- <a href="https://github.com/qwertyuiop9/land_block_sale">Github repository</a></li>
+                <li>- <a href="https://twitter.com/vendrame_and">Twitter</a></li>
+                <li>- <a href="https://www.superrisknft.com/">Website</a></li>
+                <li>- <a href="https://www.instagram.com/s_superrisk/">Instagram</a></li>
+                <li>- <a href="https://superrisk-studio.gitbook.io/skybreach-land-block-sale/">Gitbook</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4 col-xs-12 location">
+              <h4 class="mt-lg-0 mt-sm-4">Contacts</h4>
+              <p>Mail - superrisknft@gmail.com</p>
+              <p>Discord - superrisk#5988</p>
+              <p>Telegram - @vendrame_and</p>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col copyright">
+              <p class="padding_20"><small class="text-white-50">© 2022. All Rights Reserved.</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+  
 
   // Metamask button
   const connectWallet = async () => {
@@ -44,8 +130,10 @@ function App() {
       if (accounts.length !== 0) {
         setCurrentAccount(accounts[0]);
         console.log("Account 0: " + accounts[0]);
+        redirect('/Landmanagement');
       } else {
         console.log("0 account connected to Metamask extension");
+        
       }
     } catch {
       console.log("Error during Metamask connection");
@@ -92,108 +180,68 @@ function App() {
 
   }, []);
 
+
+  // VARIOUS STYLES
+  const routeLinkStyle = {
+    textDecoration: "none",
+    color: 'white',
+    fontWeight: '800',
+    fontSize: 20
+  };
+
+
   return (
     <BrowserRouter>
 
-    <div className="App" class="bg" >
+      <div className="App" class="bg" >
 
-      <AppBar position="static" sx={{ height: 130, backgroundColor: "#282c34", boxShadow: 24 }}>
+        <Routes >
 
-        <Box display="inline-flex" alignItems="center" justifyContent="space-between" sx={{ height: 100, mt: 3 }} >
-          <Box display='inline-flex' sx={{ ml: 4, alignItems: "center" }}>
-            <Box sx={{ minWidth: 100, maxWidth: 150, minHeight: 100, maxHeight: 150, mt: 2, elevation: 5, borderRadius: 100, boxShadow: 12, }} >
-              <img src={SRSlogo} alt="SRS logo" class="img-fluid" />
+          <Route exact path='/' element={
+            <Box className='partialPage' display='flex' justifyContent='center' alignItems='center'>
+              <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' >
+                <Box sx={{ maxWidth: 350 }}>
+                  <img src={SRSlogo} alt="Superrisk Studio logo" class='img-fluid' />
+                </Box>
+                <Typography className='blueGradientText' sx={{ fontSize: 130, fontWeight: 1000 }}>
+                  SKYBREACH TOOLS
+                </Typography>
+                <Typography className='blueGradientText' sx={{ mt: -4, fontSize: 80, fontWeight: 1000 }}>
+                  Land-block Sale
+                </Typography>
+                <Button className='blueGradientButton blueGradientButton--navigation' variant='contained' size='large' sx={{mt: 5, width: 400, height: 150, borderRadius: 50 }} onClick={connectWallet}>
+                  <Typography sx={{ color: 'white', fontWeight: '800', fontSize: 40}}>
+                    Connect&nbsp; wallet
+                  </Typography>
+                </Button>
+              </Box>
             </Box>
-            <Box >
-              <Typography sx={{ ml: 2, fontWeight: 'bold', fontSize: 'clamp(26px, 4vw, 40px)', textAlign: 'left' }} variant='h4' >
-                Skybreach tools
-              </Typography>
-              <Typography sx={{ ml: 2, mb: 3, fontWeight: 'normal', fontSize: 'clamp(18px, 4vw, 30px)', textAlign: 'left' }} variant='h5'>
-                Land Block Sale
-              </Typography>
-            </Box>
-          </Box>
+          } />
 
-          <Box sx={{ minWidth: 50 }} />
-
-          <Box display="inline-flex" sx={{ mr: 4, mb: 2, alignItems: "center", justifyContent: "flex-end", minHeight: 10 }}>
-
-
-            <Box >
-              {updateConnectedWallet()}
-            </Box>
-          </Box>
-        </Box>
-      </AppBar>
-
-      <Routes >
-
-      <Route exact path='/' element= {
-            <>
-            <h1>
-              Home Page
-            </h1>
+          <Route exact path='/Offermarket' element={
             <div>
-            <Link to={'/Offermarket' }> OfferMarket</Link>
+              {SkybreachAppBar()}
+              <div class="mt-5 container">
+                <HandleOffers wallet={currentAccount} />
+                <DepositedLandCard />
+              </div>
+              {SkybreachFooter()}
             </div>
+          } />
+
+          <Route exact path='/Landmanagement' element={
             <div>
-            <Link to={'/Landmanagement'}> LandManagement</Link>
+              {SkybreachAppBar()}
+              <div class="mt-5 container">
+                <CreateOffer create_offer={0} />
+                <WithdrawComponent key={currentAccount} connected_account={currentAccount} />
+              </div>
+              {SkybreachFooter()}
             </div>
-          </>
-      }/>
+          } />
 
-      <Route exact path='/Offermarket' element= {
-        <div class="mt-5 container">
-          <HandleOffers wallet={currentAccount}/>
-          <DepositedLandCard />
-        </div>
-      }/>
-
-      <Route exact path='/Landmanagement' element= {
-        <div class="mt-5 container">
-          <CreateOffer create_offer={0}/>
-          <WithdrawComponent key={currentAccount} connected_account={currentAccount} />
-        </div>
-      }/>
-
-      </Routes>
-
-        <div class="mt-5 pt-5 pb-5 footer">
-          <div class="box_container_padded">
-            <div class="d-flex justify-content-end">
-              <div class="wrapper">
-                <h2>Skybreach tools - Land Block Sale
-                </h2>
-                <div class="padding_20">
-                  <p class="pr-5 text-white-50">Skybreach tools - Land Block Sale (also shortened LBS) is a free dApp that has the aim to add a new feature by giving to the project users the possibility to sell Skybreach metaverse lands in block directly on-chain and without trust a third human part.</p>
-                </div>
-              </div>
-              <div class="col-lg-3 col-xs-12 links">
-                <h4 class="mt-lg-0 mt-sm-3">Links</h4>
-                <ul class="m-0 p-0">
-                  <li>- <a href="https://github.com/qwertyuiop9/land_block_sale">Github repository</a></li>
-                  <li>- <a href="https://twitter.com/vendrame_and">Twitter</a></li>
-                  <li>- <a href="https://www.superrisknft.com/">Website</a></li>
-                  <li>- <a href="https://www.instagram.com/s_superrisk/">Instagram</a></li>
-                  <li>- <a href="https://superrisk-studio.gitbook.io/skybreach-land-block-sale/">Gitbook</a></li>
-                </ul>
-              </div>
-              <div class="col-lg-4 col-xs-12 location">
-                <h4 class="mt-lg-0 mt-sm-4">Contacts</h4>
-                <p>Mail - superrisknft@gmail.com</p>
-                <p>Discord - superrisk#5988</p>
-                <p>Telegram - @vendrame_and</p>
-              </div>
-            </div>
-            <div class="row mt-4">
-              <div class="col copyright">
-                <p class="padding_20"><small class="text-white-50">© 2022. All Rights Reserved.</small></p>
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-
+        </Routes>
+      </div >
     </BrowserRouter>
   );
 
